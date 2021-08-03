e59035b0e15f44454eb407cab6b824933404150a
yoshi-automation <yoshi-automation@google.com>
2021-08-03 14:36:13 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-bigqueryreservation/src/main/java/com/google/cloud/bigquery/reservation/v1/stub/GrpcReservationServiceStub.java
+++ google-cloud-bigqueryreservation/src/main/java/com/google/cloud/bigquery/reservation/v1/stub/GrpcReservationServiceStub.java
@@ -876,7 +876,13 @@ public class GrpcReservationServiceStub extends ReservationServiceStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
